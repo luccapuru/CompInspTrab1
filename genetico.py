2ccef0ee72fcdf9d5393d625a821301f99ed632b
lucca <luccapuru@outlook.com>
2021-04-14 03:34:48 -0300

    metodos alternativos de selecao feitos (ate SUS)

--- genetico.py
+++ genetico.py
@@ -53,11 +53,44 @@ def Roleta(dist):
     #print("M mod:", m)
     return pop
 
-#def AtualizaPop(pop, m):
-    #m1 = [] 
-    #for i in range(len(m)):
-        #m1.append(copy.deepcopy(m[pop[i]]))
-    #return m1
+def Torneio(dist):
+    pop = []
+    for i in range(len(dist)):
+        sorteio = random.sample(range(len(dist)), 3)
+        print(sorteio)
+        if dist[sorteio[0]] >= dist[sorteio[1]] and dist[sorteio[0]] >= dist[sorteio[2]]:
+            pop.append(sorteio[0])
+        elif dist[sorteio[1]] >= dist[sorteio[0]] and dist[sorteio[1]] >= dist[sorteio[2]]:
+            pop.append(sorteio[1])
+        elif dist[sorteio[2]] >= dist[sorteio[1]] and dist[sorteio[2]] >= dist[sorteio[0]]:
+            pop.append(sorteio[2])
+    return pop
+
+def SUS(dist):
+    soma = 0
+    distaux = 0
+    for i in dist:
+        soma += i
+    dist1 = []
+    pop = []
+    for i in range(len(dist)):
+        distaux += round(dist[i]*360/soma)
+        dist1.append(distaux)
+    print(dist1)
+    sorteio = random.randint(0, 90)
+    #print("Sorteio 0", sorteio)
+    esp = 360/len(dist)
+    #agulhas = []
+    for i in range(len(dist)):
+        print("Sorteio", i, sorteio)
+        for j in range(len(dist)):
+            if sorteio < dist1[j]:
+                pop.append(j)
+                break
+        sorteio += esp
+        if sorteio > 360:
+            sorteio -= 360
+    return pop
 
 def Crossover(l, q):
     cp = random.randint(0, len(l)-1) #ponto de crossover
@@ -79,7 +112,6 @@ def Mutacao(pm, m):
                     i[j] = 1
                 else: 
                     i[j] = 0
-
 
 def Reproducao(pop, m):
     m1 = [] 
@@ -115,7 +147,9 @@ def Genetico(bitstring, pc, pm, maxGen):
     print("hMedio:", hMedio)
     print("Gen:", gen)
     while gen <= maxGen and 12 not in h: #Condicao de Parada: Max de geracoes e aptidao 
-        pop = Roleta(h) #selecao por roleta #retorna os indices da nova populacao
+        #pop = Roleta(h) #selecao por roleta #retorna os indices da nova populacao
+        #pop = Torneio(h)
+        pop = SUS(h)
         m = Reproducao(pop, m)
         Mutacao(0.02, m)
         h = Distancia(m, bitstring)
@@ -132,19 +166,3 @@ taxaMutacao = input("Digite a taxa de mutacao: ")
 taxaCross = input("Digite a taxa de Crossover: ")
 maxGen = input("Digite o maximo de geracoes: ")
 print(Genetico(lt, float(taxaCross), float(taxaMutacao), int(maxGen)))
-
-#matriz = PopulacaoRandom()
-#print("M:", matriz)
-#lt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #bitstring desejada (invertida)
-#d = Distancia(matriz, lt)
-#print("D", d)
-#pop = Roleta(d)
-#print(Roleta(d))
-# = AtualizaPop(pop, matriz)
-#print("M:", matriz)
-#matriz1 = Reproducao(pop, matriz)
-#print("M':", matriz1)
-#Mutacao(0.02, matriz1)
-#print("M:", matriz)
-#print("M'':", matriz1)
-
--- testeDistancia.py
+++ testeDistancia.py
@@ -4,8 +4,8 @@ def Distancia(m, l):
     for i,lista in enumerate(m):
         for j,num in enumerate(lista):
             if m[i][j] != l[j]:
-                print(m[i][j], l[j])
-                print(m[i][j], l[i])
+                #print(m[i][j], l[j])
+                #print(m[i][j], l[i])
                 dist[i] += 1
     return dist
 
@@ -26,4 +26,42 @@ for i,lista in enumerate(m):
 lt = [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
 
 print("M:", m)
-print(Distancia(m, lt))
+dist = Distancia(m, lt)
+print("d", dist)
+
+#pop = []
+#for i in range(len(dist)):
+    #sorteio = random.sample(range(len(dist)), 3)
+    #print(sorteio)
+    #if dist[sorteio[0]] >= dist[sorteio[1]] and dist[sorteio[0]] >= dist[sorteio[2]]:
+        #pop.append(sorteio[0])
+    #elif dist[sorteio[1]] >= dist[sorteio[0]] and dist[sorteio[1]] >= dist[sorteio[2]]:
+       # pop.append(sorteio[1])
+    #elif dist[sorteio[2]] >= dist[sorteio[1]] and dist[sorteio[2]] >= dist[sorteio[0]]:
+        #pop.append(sorteio[2])
+
+soma = 0
+distaux = 0
+for i in dist:
+    soma += i
+dist1 = []
+pop = []
+for i in range(len(dist)):
+    distaux += round(dist[i]*360/soma)
+    dist1.append(distaux)
+print(dist1)
+sorteio = random.randint(0, 90)
+#print("Sorteio 0", sorteio)
+esp = 360/len(dist)
+#agulhas = []
+for i in range(len(dist)):
+    print("Sorteio", i, sorteio)
+    for j in range(len(dist)):
+        if sorteio < dist1[j]:
+            pop.append(j)
+            break
+    sorteio += esp
+    if sorteio > 360:
+        sorteio -= 360
+
+print(pop)
